03435bb8b96b26163c6c6509d87496d0ebe6c2dc
Oleksandr Chepelev <uzi2033@gmail.com>
2024-02-24 15:30:40 +0200

    remake HW6

--- HomeWork/HW6.py
+++ HomeWork/HW6.py
@@ -3,6 +3,7 @@ my_list=[12,3,4,10]
 #my_list=[]
 #my_list=[12, 3, 4, 10, 8]
 if len(my_list) > 1:
-    my_list[-1], my_list[0] = my_list[0], my_list[-1]
+    a = my_list.pop(-1)
+    my_list.insert(0,a)
     print(my_list)
 else: print(my_list)
